1ac8d34453a2c7c239b8f5cc1c2af6f36d932577
JhonSon12 <jhonpacalagua@cbit-online.com>
2025-05-14 16:41:30 -0500

    primer reto

--- Reto1.py
+++ Reto1.py
@@ -1,13 +1,27 @@
 """
  * EJERCICIO:
- * - Crea un comentario en el codigo y coloca la URL del sitio web oficial del
- *   lenguaje de programacion que has seleccionado.
+ * - Crea un comentario en el código y coloca la URL del sitio web oficial del
+ *   lenguaje de programación que has seleccionado.
  * - Representa las diferentes sintaxis que existen de crear comentarios
- *   en el lenguaje (en una linea, varias...).
+ *   en el lenguaje (en una línea, varias...).
  * - Crea una variable (y una constante si el lenguaje lo soporta).
  * - Crea variables representando todos los tipos de datos primitivos
  *   del lenguaje (cadenas de texto, enteros, booleanos...).
- * - Imprime por terminal el texto: "Hola, [y el nombre de tu lenguaje]
- """"
+ * - Imprime por terminal el texto: "¡Hola, [y el nombre de tu lenguaje]
+ """
+
+#https://www.python.org/
+
+numero = 10 # Variable
+PI = 3.14 # Constante
+
+# Tipos de datos primitivos
+cadena = "Hola" # Cadena de texto   
+entero = 5 # Entero
+booleano = True # Booleano
+flotante = 3.14 # Flotante
+# Imprimir en consola
+print("¡Hola, Python!") # Imprime el mensaje
+
 
 
